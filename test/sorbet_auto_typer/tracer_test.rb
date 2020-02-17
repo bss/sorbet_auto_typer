b4a426386c9fed8ffd6fd405aec4d127a96a8bca
Bo Stendal Sorensen <bo@stendal-sorensen.net>
2020-02-17 14:16:51 -0500

    Improve tracer test to use new API and test more types of params/returns

--- test/sorbet_auto_typer/tracer_test.rb
+++ test/sorbet_auto_typer/tracer_test.rb
@@ -6,8 +6,8 @@ class TracerTest < Minitest::Test
 
   sig { void }
   def test_tracer_traces_method_calls
-    output = StringIO.new
-    tracer = SorbetAutoTyper::Tracer.new(output, filter_path=Dir.pwd)
+    temp_file = Tempfile.new
+    tracer = SorbetAutoTyper::Tracer.new(temp_file.path, filter_path=Dir.pwd)
     tracer.start!
     HelperClass.bar(27)
     HelperClass.bar
@@ -16,6 +16,15 @@ class TracerTest < Minitest::Test
     HelperClass.new.foo(true)
     TypedHelperClass.new.method_with_signature # Should not show up below since it's typed
     HelperModule::Test.foo
+    HelperClass.blarp
+    HelperModule.hash_tester(['a', 'b', 'c', 'd', 'e'])
+    HelperModule.hash_tester(['lol', 'foo', 45, 'bar', 24], { a: [1, 'ff', { ['a', 3] => 4}] })
+    HelperModule.range_tester(1..4)
+    HelperModule.range_tester(4..10)
+    HelperModule::Test.bar
+    HelperModule::Test.const_in_singleton_class
+    HelperModule.empty_array
+    HelperClass::ASelfClass.new.something
     tracer.stop!
 
     expected_output = [
@@ -31,8 +40,29 @@ class TracerTest < Minitest::Test
       "R|HelperClass|instance|foo|D;Integer",
       "C|HelperModule::Test|class|foo",
       "R|HelperModule::Test|class|foo|D;String",
+      "C|HelperClass|class|blarp",
+      "R|HelperClass|class|blarp|D;HelperClass::AnotherClass",
+      "C|HelperModule|class|hash_tester|arr|A;(D;String)|reverse|D;FalseClass",
+      "R|HelperModule|class|hash_tester|H;(D;String);(D;Integer)",
+      "C|HelperModule|class|hash_tester|arr|A;(D;String;D;Integer)|reverse|H;(D;Symbol);(A;(D;Integer;D;String;H;(A;(D;String;D;Integer));(D;Integer)))",
+      "R|HelperModule|class|hash_tester|H;(D;String;D;Integer);(D;Integer;D;String)",
+      "C|HelperModule|class|range_tester|range|R;(D;Integer)",
+      "R|HelperModule|class|range_tester|S;(D;Integer;D;String)",
+      "C|HelperModule|class|range_tester|range|R;(D;Integer)",
+      "R|HelperModule|class|range_tester|R;(D;Date)",
+      "C|HelperModule::Test|class|bar",
+      "R|HelperModule::Test|class|bar|D;NilClass",
+      "C|HelperModule::Test|class|const_in_singleton_class",
+      /R|HelperModule::Test|class|const_in_singleton_class|D;.*::PrivateClass/,
+      "C|HelperModule|class|empty_array",
+      "R|HelperModule|class|empty_array|A;()",
+      "C|HelperClass::ASelfClass|instance|something",
+      "R|HelperClass::ASelfClass|instance|something|A;(D;String;D;Integer)",
     ]
-    actual_output = output.string.split("\n")
-    assert_equal expected_output, actual_output
+    actual_output = File.read(temp_file.path).split("\n")
+    assert_equal(expected_output.size, actual_output.size)
+    expected_output.each_with_index do |expected, idx|
+      assert_match expected, actual_output[idx]
+    end
   end
 end
